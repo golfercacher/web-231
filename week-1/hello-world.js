39fb9b054468011e67e2a24ba61ff15943f9a644
Sean Kenney <golfercacher@gmail.com>
2019-11-27 16:45:33 -0600

    adding week one hello world

---
+++ week-1/hello-world.js
@@ -0,0 +1,12 @@
+/*
+=================================================
+; Title: Assignment 1.3
+; Author: Professor Krasso - via instructions for this assignment
+; Date: 27 November 2019
+; Modified by: Sean Kenney
+; Description: This program displays a message to the console
+=================================================
+*/
+
+//display a message on the console that includes my name
+console.log("Hello World, my name is Sean Kenney");
