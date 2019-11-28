f189f114008feef4e20d2cabb15bbad1daaf0de2
Sean Kenney <golfercacher@gmail.com>
2019-11-27 20:26:04 -0600

    posting fixes to lintel

---
+++ week-1/lintel-fix-assignment-1.1.js
@@ -0,0 +1,35 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Jeff Lintel
+; Date: 26 November 2019
+; Description: This program demonstrates the
+; use of JavaScript values and variables. It
+should print the sum of 2 variables to the 
+console.
+;===========================================
+*/ 
+
+/*Original code was using == and should be using =.
+; == is not an assignment operator, = is.
+; as written the code was essentially stating 
+; is variable firstNumber equal to 16
+;   Original code
+;   var firstNumber == 16;
+*/
+
+//fixed code that states firstNumber is assigned the value of 16
+var firstNumber = 16;
+
+var secondNumber = 25;
+
+/* Original code was using a capital C for console
+; my debugger threw an error and I changed it to a lower 
+; case c and it executed.  
+; it is case sensitive.
+; original code
+; Console.log(firstNumber + secondNumber);
+*/
+
+//fixed code - capital C change to lower case c
+console.log(firstNumber + secondNumber);
