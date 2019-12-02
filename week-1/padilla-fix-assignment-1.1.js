fe2443efd87ce42e5af383de9540b786237e398a
Sean Kenney <golfercacher@gmail.com>
2019-12-02 11:39:46 -0600

    cleaning up

--- week-1/padilla-fix-assignment-1.1.js
+++ week-1/padilla-fix-assignment-1.1.js
@@ -6,17 +6,19 @@
 ; Modified By: <Your name>
 ; Description: This program is created to contain two errors
 ;===========================================
-*/ 
+*/
+
+// adding a comment to force a change that I can move up to my repo
 
 var person = {
     name: null,
     speak: function(){
         if(name != null){
-            console.log("Hello my name is " + name);    
+            console.log("Hello my name is " + name);
         }else{
             console.log("Please assign a value to name.");
         }
-        
+
     }
 }
 
