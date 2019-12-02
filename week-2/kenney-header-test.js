e58a1dc294314b30946a76f2d5acc4da885793cf
Sean Kenney <golfercacher@gmail.com>
2019-12-02 13:27:26 -0600

    Excercise 2.2 Header

---
+++ week-2/kenney-header-test.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title:  kenney-header-test.js
+; Author: Professor Krasso
+; Date:   02 December 2019
+; Modified By: Sean Kenney
+; Description: Displays a formatted header
+;===========================================
+*/
+const header = require('../kenney-header.js');
+
+console.log(header.display("Sean", "Kenney", "Excercise 2.2 Header"));
