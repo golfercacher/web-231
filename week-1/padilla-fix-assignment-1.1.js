325410f7a926c1ff7c87ca4d6fe32a0df11128b8
Sean Kenney <golfercacher@gmail.com>
2019-12-02 11:09:55 -0600

    cleaning up

---
+++ week-1/padilla-fix-assignment-1.1.js
@@ -0,0 +1,25 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Angel Padilla
+; Date: 11-27-2019
+; Modified By: <Your name>
+; Description: This program is created to contain two errors
+;===========================================
+*/ 
+
+var person = {
+    name: null,
+    speak: function(){
+        if(name != null){
+            console.log("Hello my name is " + name);    
+        }else{
+            console.log("Please assign a value to name.");
+        }
+        
+    }
+}
+
+
+person.speak(person.name);
+person = "Angel";
