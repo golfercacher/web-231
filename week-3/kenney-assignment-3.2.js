bf23369de78d5e6a9d621b3ff9c685b5ab2a13f5
Sean Kenney <golfercacher@gmail.com>
2019-12-08 14:42:16 -0600

    updates to assignment 3.2

--- week-3/kenney-assignment-3.2.js
+++ week-3/kenney-assignment-3.2.js
@@ -1,11 +1,11 @@
 /*
 ============================================
 ; Title:  kenney-assignment-3.1.js
-; Author: Professor Krasso - referenced starter code
+; Author: Professor Krasso - referenced starter code and video
 ; Date:   08 December 2019
 ; Modified By: Sean Kenney
-; Description:
-;
+; Description: Pattern matching.  Functions to check for equality
+; and to then output a message based on equality.
 ;===========================================
 */
 
@@ -33,6 +33,37 @@ const vehicleBike2 = "Bike";
 const numberFour = "Four";
 const numberThree = "Three";
 
+/*
+; function that compares two values and if they are
+; equal returns true and if they aren't equal
+; returns false
+*/
+function match (firstValue, secondValue) {
+  if (firstValue === secondValue) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/*
+; A function called logMismatch with two parameters,
+; if the values are not equal a message will be displayed saying they are not equal.
+; the messages will display the parameters being compared.
+*/
+function logMismatch (firstParameter, secondParameter) {
+    console.log(firstParameter + " and " + secondParameter + " do not match!");
+}
+
+/*
+; A function called logMatch with two parameters,
+; if the values are equal a message will be displayed saying they are equal.
+; the messages will display the parameters being compared.
+*/
+function logMatch (firstParameter, secondParameter) {
+    console.log(firstParameter + " and " + secondParameter + " do match!");
+}
+
 // output to test match function
 console.log(match("A","B"));
 console.log(match(2,2));
@@ -59,41 +90,4 @@ if (match(numberFour, numberThree)) {
   logMismatch(numberFour, numberThree);
 }
 
-/*
-; function that compares two values and if they are
-; equal returns true and if they aren't equal
-; returns false
-*/
-
-function match (firstValue, secondValue) {
-  if (firstValue === secondValue) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-
-/*
-; A function called logMismatch with two parameters,
-; if the values are not equal a message will be displayed saying they are not equal.
-; the messages will display the parameters being compared.
-*/
-function logMismatch (firstParameter, secondParameter) {
-  if (firstParameter !== secondParameter) {
-    console.log(firstParameter + " and " + secondParameter + " do not match!");
-  }
-}
-
-/*
-; A function called logMatch with two parameters,
-; if the values are equal a message will be displayed saying they are equal.
-; the messages will display the parameters being compared.
-*/
-function logMatch (firstParameter, secondParameter) {
-  if (firstParameter === secondParameter) {
-    console.log(firstParameter + " and " + secondParameter + " do match!");
-  }
-}
-
-
+//end of program
