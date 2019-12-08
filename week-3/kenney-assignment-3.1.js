0589489ca3a80d1585385f1fb643605b4ea23537
Sean Kenney <golfercacher@gmail.com>
2019-12-08 13:07:02 -0600

    updates to assignment 3.1

--- week-3/kenney-assignment-3.1.js
+++ week-3/kenney-assignment-3.1.js
@@ -15,16 +15,29 @@
 ;===========================================
 */
 
+/*
+; include header called kenney-header.js
+; This header will display my fist and last name
+; and the assignment name
+*/
+
+const header = require('../kenney-header.js');
+
+console.log(header.display("Sean", "Kenney", "Exercise 3.1 Control Statements"));
+/*
+**********end including of header
+*/
+
 var currentAge = 23;
 
 switch (true) {
-  case currentAge >=0 && currentAge <= 20;
-      console.log("Below legal drinking age");
-      break;
-  case currentAge > 20 && currentAge <= 100:
-      console.log("Above legal drinking age");
+  case currentAge >=0 && currentAge <= 20:
+      console.log("\nBelow legal drinking age");
       break:
-  default:
-      console.log("Age needs to be a value of 0-100");
+  case currentAge > 20 && currentAge <= 100:
+      console.log("\nAbove legal drinking age");
+      break;
+  default;
+      console.log("\nAge needs to be a value of 0-100");
 
 }
