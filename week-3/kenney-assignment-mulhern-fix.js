202fe8dc9090be5ac2c0fb1672a555ca6f1dc0ef
Sean Kenney <golfercacher@gmail.com>
2019-12-10 15:46:35 -0600

    pushing fix to mulhern 3.1

---
+++ week-3/kenney-assignment-mulhern-fix.js
@@ -0,0 +1,22 @@
+/*
+============================================
+; Title: Discussion Board 3.1
+; Author: Brendan Mulhern
+; Date: 10 December 2019
+; Modified By: Sean Kenney
+; Description: This is a loop program which contains two errors.
+;===========================================
+*/
+
+// Defines function isItTwenty which asks the question; is the number 20?
+function isItTwenty(n) {
+  if (n === 20 ) {
+  console.log("It's 20");
+} else {
+  console.log("It's not 20");
+}
+}
+
+// Runs the function isItTwenty.
+isItTwenty(20);
+isItTwenty(19);
