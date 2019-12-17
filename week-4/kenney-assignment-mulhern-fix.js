5c3ac0ef0c9c0f3c5403598a527a1a03eee51366
Sean Kenney <golfercacher@gmail.com>
2019-12-17 12:02:05 -0600

    adding mulhern debug

---
+++ week-4/kenney-assignment-mulhern-fix.js
@@ -0,0 +1,16 @@
+/*
+============================================
+; Title: Discussion Board 4.1
+; Author: Brendan Mulhern
+; Date: 17 December 2019
+; Modified By: Brendan Mulhern
+; Description: This is a program that has two errors in it.
+;===========================================
+*/
+// Defines Array
+var arrayItems = [1, 2, 5, 7, 9];
+// For Loop printing out the array
+for (i=0; i<arrayItems.length; i++) {
+// Prints out the array
+  console.log(arrayItems[i]);
+};
